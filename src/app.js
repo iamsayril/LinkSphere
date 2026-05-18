b5f9c0b616489ed0367606a207046c858da1224c
iamsayril <iamsayril@gmail.com>
2026-05-18 15:44:04 +0800

    aa

--- src/app.js
+++ src/app.js
@@ -33,6 +33,7 @@ const allowedOrigins = [
   'http://localhost:5500',
   'http://127.0.0.1:3000',
   'http://localhost:3000',
+  'https://linksphere-frontend.netlify.app',
   process.env.CORS_ORIGIN,
 ].filter(Boolean);
 
