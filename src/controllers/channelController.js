ee5361349ea0850d8a9874784bb1a49e9e5245db
iamsayril <iamsayril@gmail.com>
2026-05-21 14:39:09 +0800

    EW

--- src/controllers/channelController.js
+++ src/controllers/channelController.js
@@ -290,7 +290,7 @@ const deleteChannel = async (req, res) => {
     // Get channel to find workspace_id
     const { data: channel } = await supabaseAdmin
       .from('channel')
-      .select('workspace_id')
+      .select('workspace_id, name')
       .eq('channel_id', channelId)
       .single();
 
